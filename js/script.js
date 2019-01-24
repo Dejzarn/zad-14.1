01aecbf904f136dff50d2a5eed9cb76a5e98f0c6
Andrzej Grabowski <andgrabowski@gmail.com>
2019-01-24 22:07:41 +0100

    Add mustache to script.js

--- js/script.js
+++ js/script.js
@@ -1,19 +1,32 @@
-var elem = document.querySelector('.main-carousel');
+    // mustache
 
-var flkty = new Flickity(elem, {
-    // options
-    cellAlign: 'left',
-    contain: true,
-    pageDots: false,
-    hash: true
-});
+    var templateSlide = document.getElementById('template-carousel').innerHTML;
+    var carousel = document.querySelector('.main-carousel');
 
-var btnRestart = document.querySelector('.restart-button');
-btnRestart.addEventListener('click', function () {
-    flkty.select(0);
-});
-var progressBar = document.querySelector('.progress-bar');
-flkty.on('scroll', function (progress) {
-    progress = Math.max(0, Math.min(1, progress));
-    progressBar.style.width = progress * 100 + '%';
-});
+    Mustache.parse(templateSlide);
+    var renderedTemplates = '';
+
+    for (var i = 0; i < data.length; i++) {
+        renderedTemplates += Mustache.render(templateSlide, data[i]);
+    }
+    carousel.innerHTML = renderedTemplates;
+
+    var elem = document.querySelector('.main-carousel');
+
+    var flkty = new Flickity(elem, {
+        // options
+        cellAlign: 'left',
+        contain: true,
+        pageDots: false,
+        hash: true
+    });
+
+    var btnRestart = document.querySelector('.restart-button');
+    btnRestart.addEventListener('click', function () {
+        flkty.select(0);
+    });
+    var progressBar = document.querySelector('.progress-bar');
+    flkty.on('scroll', function (progress) {
+        progress = Math.max(0, Math.min(1, progress));
+        progressBar.style.width = progress * 100 + '%';
+    });
