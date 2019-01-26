6c82d6a9a272d1837926930851bc7de4602309d0
Andrzej Grabowski <andgrabowski@gmail.com>
2019-01-26 20:08:39 +0100

    Set zoom for map

--- js/script.js
+++ js/script.js
@@ -1,47 +1,57 @@
-    var templateSlide = document.getElementById('template-carousel').innerHTML;
-    var carousel = document.querySelector('.main-carousel');
+var templateSlide = document.getElementById('template-carousel').innerHTML;
+var carousel = document.querySelector('.main-carousel');
 
-    Mustache.parse(templateSlide);
-    var renderedTemplates = '';
+Mustache.parse(templateSlide);
+var renderedTemplates = '';
 
-    for (var i = 0; i < data.length; i++) {
-        renderedTemplates += Mustache.render(templateSlide, data[i]);
-    }
-    carousel.innerHTML = renderedTemplates;
+for (var i = 0; i < data.length; i++) {
+    renderedTemplates += Mustache.render(templateSlide, data[i]);
+}
+carousel.innerHTML = renderedTemplates;
 
-    var elem = document.querySelector('.main-carousel');
+var elem = document.querySelector('.main-carousel');
 
-    var flkty = new Flickity(elem, {
-        cellAlign: 'left',
-        contain: true,
-        pageDots: false,
-        hash: true
+var flkty = new Flickity(elem, {
+    cellAlign: 'left',
+    contain: true,
+    pageDots: false,
+    hash: true
+});
+
+var btnRestart = document.querySelector('.restart-button');
+btnRestart.addEventListener('click', function () {
+    flkty.select(0);
+});
+var progressBar = document.querySelector('.progress-bar');
+flkty.on('scroll', function (progress) {
+    progress = Math.max(0, Math.min(1, progress));
+    progressBar.style.width = progress * 100 + '%';
+});
+
+
+window.initMap = function () {
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 2,
+        center: data[0].coords
     });
 
-    var btnRestart = document.querySelector('.restart-button');
-    btnRestart.addEventListener('click', function () {
-        flkty.select(0);
-    });
-    var progressBar = document.querySelector('.progress-bar');
-    flkty.on('scroll', function (progress) {
-        progress = Math.max(0, Math.min(1, progress));
-        progressBar.style.width = progress * 100 + '%';
-    });
+    var markers = [];
 
+    for (let i = 0; i < data.length; i++) {
+        markers[i] = new google.maps.Marker({
+            position: data[i].coords,
+            map: map
 
-    window.initMap = function () {
-        var map = new google.maps.Map(document.getElementById('map'), {
-            zoom: 2,
-            center: data[0].coords
         });
 
-        var markers = [];
+        markers[i].addListener('click', function () {
+            flkty.select(i);
+        });
 
-        for (let i = 0; i < data.length; i++) {
-            markers[i] = new google.maps.Marker({
-                position: data[i].coords,
-                map: map
-
-            });
-        }
     }
+
+    flkty.on('change', function (a) {
+        map.panTo(data[a].coords);
+        map.setZoom(2);
+    });
+}
